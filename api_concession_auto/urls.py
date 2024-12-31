bff8b82d6e24cd751e1877d80edcc969e74f5cda
hitochix06 <pachara.philibert@hotmail.fr>
2024-12-31 18:09:03 +0100

    Refactoriser les fichiers de configuration et les URL pour utiliser le framework Django REST

--- api_concession_auto/urls.py
+++ api_concession_auto/urls.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_nested import routers
-from api.views import ConcessionViewSet, VehiculeViewSet
+from rest_framework.routers import DefaultRouter
+from api import views
 
-router = routers.DefaultRouter()
-router.register(r'concessions', ConcessionViewSet)
-
-concessions_router = routers.NestedDefaultRouter(router, r'concessions', lookup='concession')
-concessions_router.register(r'vehicules', VehiculeViewSet, basename='concession-vehicules')
+router = DefaultRouter()
+router.register(r'concessions', views.ConcessionViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('', include(concessions_router.urls)),
 ] 
