daf830c8b8649a01cfd4cbe6c1b407430c2f333c
hitochix06 <pachara.philibert@hotmail.fr>
2025-01-01 10:24:09 +0100

    Refactoriser les fichiers de configuration et les URL pour utiliser le framework Django REST

--- api/views.py
+++ api/views.py
@@ -8,4 +8,13 @@ class ConcessionViewSet(viewsets.ModelViewSet):
 
 class VehiculeViewSet(viewsets.ModelViewSet):
     queryset = Vehicule.objects.all()
-    serializer_class = VehiculeSerializer 
+    serializer_class = VehiculeSerializer
+
+class ConcessionVehiculesViewSet(viewsets.ModelViewSet):
+    serializer_class = VehiculeSerializer
+
+    def get_queryset(self):
+        return Vehicule.objects.filter(concession_id=self.kwargs['concession_pk'])
+
+    def perform_create(self, serializer):
+        serializer.save(concession_id=self.kwargs['concession_pk']) 
--- api_concession_auto/urls.py
+++ api_concession_auto/urls.py
@@ -5,8 +5,15 @@ from api import views
 
 router = DefaultRouter()
 router.register(r'concessions', views.ConcessionViewSet)
+router.register(r'vehicules', views.VehiculeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('concessions/<int:concession_pk>/vehicules/', 
+         views.ConcessionVehiculesViewSet.as_view({'get': 'list', 'post': 'create'}),
+         name='concession-vehicules-list'),
+    path('concessions/<int:concession_pk>/vehicules/<int:pk>/',
+         views.ConcessionVehiculesViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
+         name='concession-vehicules-detail'),
 ] 
